66a02a9bcef5f9aa3a8ecb22c0dd705bb274ca7d
zeng ge <sweetifan@163.com>
2018-11-16 15:35:30 +0800

    测试

--- src/main/java/com/github/zengge95/HelloWorld.java
+++ src/main/java/com/github/zengge95/HelloWorld.java
@@ -2,6 +2,6 @@ package com.github.zengge95;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("this is a test.");
+        System.out.println("This is a test.");
     }
 }
