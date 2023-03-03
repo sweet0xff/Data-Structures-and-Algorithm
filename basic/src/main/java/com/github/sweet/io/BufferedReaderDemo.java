d9a53e10ecb54fbea6c37623a778238f7ae3c167
sweet0xff <sweetifan@qq.com>
2023-03-03 15:32:51 +0800

    some code

--- basic/src/main/java/com/github/sweet/io/BufferedReaderDemo.java
+++ basic/src/main/java/com/github/sweet/io/BufferedReaderDemo.java
@@ -1,9 +1,8 @@
 package com.github.sweet.io;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.InputStreamReader;
+import org.junit.Test;
+
+import java.io.*;
 
 /**
  * @author sweet
@@ -11,10 +10,21 @@ import java.io.InputStreamReader;
  * @date 2021/10/19 09:14
  */
 public class BufferedReaderDemo {
-    public static void main(String[] args) throws Exception{
+
+    @Test
+    public void test01() throws Exception{
         InputStreamReader reader = new InputStreamReader(new FileInputStream("a.txt"), "UTF-8");
         reader.read();
         BufferedReader bufferedReader  = new BufferedReader(new InputStreamReader(new FileInputStream("a.txt"), "UTF-8"));
         bufferedReader.read();
+    }
+
+    @Test
+    public void test02() throws IOException {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("xx.demo"));
+        int content = 0;
+        while ((content = bufferedInputStream.read()) != -1) {
+
+        }
     }
 }
